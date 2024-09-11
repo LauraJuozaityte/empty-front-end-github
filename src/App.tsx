f6cd381f53951a276eb72cf79d8b643162e60318
Juozaityte <laura.juozaityte@accenture.com>
2024-09-11 13:23:58 +0300

    Commit to remote.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ export function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Text paragraph fhmhy </p>
+        <p>Text paragraph 1</p>
+        <p>Text paragraph 2</p>
+        <p>Text paragraph 3</p>
       </header>
     </div>
   );
